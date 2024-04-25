eb8b177810fb32e048788a4d2712f82a1876fdda
siwar said <siwarsaid8@gmail.com>
2024-04-25 10:24:02 +0200

    dictionnaires

---
+++ Dictionnaires/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Dictionnaires
+{ 
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<char,string> dic = new Dictionary<char,string>();
+            dic.Add('a', "fraise");
+            dic.Add('b', "banane");
+            dic.Add('c', "pomme");
+            dic.Remove('c');
+            
+        }
+    }
+}
